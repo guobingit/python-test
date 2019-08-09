7c1796e14809654bf420dc43ad49d64cf46b97f5
guobingit <331957487@qq.com>
2019-08-09 15:47:14 +0800

    hello world

---
+++ crawler/CaptureImages.py
@@ -0,0 +1,29 @@
+import urllib.request
+import re
+import os
+import urllib
+
+## 网页抓取
+#根据给定的网址来获取网页详细信息，得到的html就是网页的源代码
+def getHtml(url):
+    page = urllib.request.urlopen(url)
+    html = page.read()
+    return html.decode('UTF-8')
+
+def getImg(html):
+    reg = r'src="(.+?\.jpg)"'
+    imgre = re.compile(reg)
+    imglist = imgre.findall(html)  # 表示在整个网页中过滤出所有图片的地址，放在imglist中
+    x = 0
+    path = 'G:\\data\\test_imges'
+    # 将图片保存到文件夹中，如果没有test文件夹则创建
+    if not os.path.isdir(path):
+        os.makedirs(path)
+    paths = path + '\\'  # 保存在test路径下
+    for imgurl in imglist:
+        urllib.request.urlretrieve(imgurl,'{0}{1}.jpg'.format(paths,x))  #打开imglist中保存的图片网址，并下载图片保存在本地，format格式化字符串
+        x = x + 1
+    return imglist
+
+html = getHtml("http://tieba.baidu.com/p/2460150866")#获取该网址网页详细信息，得到的html就是网页的源代码
+print (getImg(html)) #从网页源代码中分析并下载保存图片
